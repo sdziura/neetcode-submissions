2b733aa9e8377c659feeaeb0ee84159f4f0d06cc
Sebastian Dziura <sabek12@gmail.com>
2026-09-07 20:50:13 +0200

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dictA = {}
+        if len(s) != len(t):
+            return False
+
+        for letter in s:
+            if letter in dictA:
+                dictA[letter] += 1
+            else:
+                dictA[letter] = 1
+        
+        for letter in t:
+            if letter not in dictA:
+                return False
+            dictA[letter] -= 1
+        
+        for count in dictA.values():
+            if count != 0:
+                return False
+        
+        return True
