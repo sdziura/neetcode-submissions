0ddf0cbee924e70fcd1c0cef31f5ad4e8d1caa86
Sebastian Dziura <sabek12@gmail.com>
2026-09-09 09:38:19 +0200

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def sortFromMin(self, e):
+        return e[1]
+
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        occurences = dict()
+        top = list()
+        
+        for num in nums:
+            if num in occurences:
+                occurences[num] += 1
+            else:
+                occurences[num] = 1
+        
+        for i in range(k):
+            top.append(occurences.popitem())
+        top.sort(key=self.sortFromMin)
+
+        for (key, value) in occurences.items():
+            if value > top[0][1]:
+                top[0] = (key, value)
+                top.sort(key=self.sortFromMin)
+        return [t[0] for t in top]
+                
+
+
